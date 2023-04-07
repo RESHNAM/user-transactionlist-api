226d5b5a519653ef38292fd2ded1b28fb2dbcf85
Renney <renney003@gmail.com>
2023-04-07 22:11:41 +0300

    use both names as presence validation in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_many :transactions, dependent: :destroy
 
   # validations
-  validates_presence_of :phone_number, :email_address
+  validates_presence_of :first_name, :last_name
 end
