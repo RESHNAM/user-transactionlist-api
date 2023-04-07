9c16bfa51b5179640059696a5ef6be4426605dd3
Renney <renney003@gmail.com>
2023-04-07 22:13:31 +0300

    Update both User and Transaction schemas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_07_093657) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_163353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_07_093657) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "total"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
