4160e27851b16f897268ff96174179ffaa3d87a4
Renney <renney003@gmail.com>
2023-04-11 13:07:04 +0300

    Fix the auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_07_163353) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_100516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "app_users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.decimal "credit"
