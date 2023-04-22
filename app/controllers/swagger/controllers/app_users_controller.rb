1a4bcb4af9333715f13e6c0572289e4316e1391c
Renney <renney003@gmail.com>
2023-04-22 12:03:43 +0300

    fix indentation

--- app/controllers/swagger/controllers/app_users_controller.rb
+++ app/controllers/swagger/controllers/app_users_controller.rb
@@ -4,21 +4,21 @@ class Swagger::Controllers::AppUsersController
     include Swagger::Blocks
   
     swagger_path '/users' do
-      operation :post do
-        key :description, 'Creates a new application user in the system'
-        key :tags, [
-          'app_user'
-        ]
+        operation :post do
+            key :description, 'Creates a new application user in the system'
+            key :tags, [
+                'app_user'
+            ]
   
-        parameter do
-          key :name, :email_address
-          key :in, :body
-          key :description, 'Email and password information of the new user'
-          key :required, true
-          schema do
-            key :'$ref', :AppUserInput
-          end
+            parameter do
+                key :name, :email_address
+                key :in, :body
+                key :description, 'Email and password information of the new user'
+                key :required, true
+                schema do
+                    key :'$ref', :AppUserInput
+                end
+            end
         end
-      end
     end
-  end
+end
--- app/controllers/swagger_controller.rb
+++ app/controllers/swagger_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SwaggerController < ActionController::Base
-    def index
-      render html: nil, layout: 'swagger'
-    end
+  def index
+    render html: nil, layout: 'swagger'
   end
+end
